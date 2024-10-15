2cde6846ac23471c0cd80eea918c8bb706881104
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-10-15 19:20:07 +0530

    feat: Implement the dashboard layout

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
+import { DottedSeperator } from "./dotted-seperator"
+import { Navigation } from "./navigation"
+
+export const Sidebar = () => {
+    return (
+        <aside className="h-full bg-neutral-100 p-4 w-full">
+            <Link href="/">
+                <div className="flex items-center space-x-2">
+                    <Image src={"/logo.svg"} alt="Logo" width={40} height={40} />
+                    <h2 className="text-xl font-bold">Task Forge</h2>
+                </div>
+            </Link>
+            <DottedSeperator className="my-4" />
+            <Navigation />
+        </aside>
+    )
+}

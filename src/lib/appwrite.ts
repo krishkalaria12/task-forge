d7d1bce49aea27504099f892c39e3c98c7802762
CodeWithKrish12 <codewithkrish12@gmail.com>
2024-10-21 22:08:37 +0530

    feat: Implement the member actions in the workspace

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -5,8 +5,8 @@ import {
     Client,
     Account,
     Databases,
+    Users,
     // Storage,
-    // Users,
 } from "node-appwrite"
 
 import { envKeys } from "@/lib/env"
@@ -44,6 +44,9 @@ export async function createAdminClient(){
     return {
         get account(){
             return new Account(client);
+        },
+        get users(){
+            return new Users(client);
         },
     };
 }
